5f9214234f19482e039b9f0f017c528c9aabbfa0
Gwankie <2014681804@qq.com>
2022-05-13 23:00:00 +0800

    First commit

---
+++ src/main/java/com/service/DiscussmingshituijianService.java
@@ -0,0 +1,30 @@
+package com.service;
+
+import com.baomidou.mybatisplus.mapper.Wrapper;
+import com.baomidou.mybatisplus.service.IService;
+import com.utils.PageUtils;
+import com.entity.DiscussmingshituijianEntity;
+import java.util.List;
+import java.util.Map;
+import com.entity.vo.DiscussmingshituijianVO;
+import org.apache.ibatis.annotations.Param;
+import com.entity.view.DiscussmingshituijianView;
+
+
+
+public interface DiscussmingshituijianService extends IService<DiscussmingshituijianEntity> {
+
+    PageUtils queryPage(Map<String, Object> params);
+    
+   	List<DiscussmingshituijianVO> selectListVO(Wrapper<DiscussmingshituijianEntity> wrapper);
+   	
+   	DiscussmingshituijianVO selectVO(@Param("ew") Wrapper<DiscussmingshituijianEntity> wrapper);
+   	
+   	List<DiscussmingshituijianView> selectListView(Wrapper<DiscussmingshituijianEntity> wrapper);
+   	
+   	DiscussmingshituijianView selectView(@Param("ew") Wrapper<DiscussmingshituijianEntity> wrapper);
+   	
+   	PageUtils queryPage(Map<String, Object> params,Wrapper<DiscussmingshituijianEntity> wrapper);
+   	
+}
+
